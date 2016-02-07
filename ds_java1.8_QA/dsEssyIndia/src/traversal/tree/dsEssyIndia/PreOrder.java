5e3fab3b8ed37acd8352d0b3f3858b80e60237ba
essyindia <essyindia@gmail.com>
2016-02-07 19:32:59 +0530

    Tree Traversal Increment
    PreOrder Traversal

--- ds_java1.8_QA/dsEssyIndia/src/traversal/tree/dsEssyIndia/PreOrder.java
+++ ds_java1.8_QA/dsEssyIndia/src/traversal/tree/dsEssyIndia/PreOrder.java
@@ -1,5 +1,36 @@
 package traversal.tree.dsEssyIndia;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import tree.dsEssyIndia.TreeNode;
+
 public class PreOrder {
+	
+	Queue objQ;
+	
+	void preOrderTraversal(TreeNode root){
+		
+		objQ=new LinkedList<TreeNode>();
+		objQ.add(root);
+		TreeNode node;
+		
+		while(!objQ.isEmpty()){
+			node=(TreeNode) objQ.poll();
+			if(node==null){
+				
+			}else{
+				System.out.println(node.getData());
+			}
+
+			if(node.getLeft()!=null){
+				objQ.add(node.getLeft());
+				
+			}else if(node.getRight()!=null){  
+				objQ.add(node.getRight());
+			}
+		}
+		
+	}
 
 }
